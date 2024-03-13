1a4ba515c49d1b5d18f7ed2c406f2cafbea8bc67
polserull <fatherofno@gmail.com>
2024-03-13 02:17:15 +0000

    pass 3st stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,15 +21,15 @@ public class Main {
       InputStream in = clientSocket.getInputStream();
       String[] msg = in.toString().split(" ");
 
-      if (Objects.equals(msg[0], "GET")) { // GET REQUEST
-        if (Objects.equals(msg[1], "/")) {
-          sendCode(clientSocket, "HTTP/1.1 200 OK\r\n\r\n");
-        } else {
-          sendCode(clientSocket, "HTTP/1.1 404 Not Found\r\n\r\n");
-        }
+      System.out.println(msg[1]);
+      System.out.println(Objects.equals(msg[1], "/"));
+
+      if (Objects.equals(msg[1], "/")) {
+        sendCode(clientSocket, "HTTP/1.1 200 OK\r\n\r\n");
+      } else {
+        sendCode(clientSocket, "HTTP/1.1 404 Not Found\r\n\r\n");
       }
 
-      in.close();
       System.out.println("accepted new connection");
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());

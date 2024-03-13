d99567400b6aa8106ade66f9f799609f5475d6b9
polserull <fatherofno@gmail.com>
2024-03-13 02:57:20 +0000

    pass 4st stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -23,6 +23,7 @@ public class Main {
 
       if (re[1].equals("/")) {
         sendCode(clientSocket, "HTTP/1.1 200 OK\r\n\r\n");
+      } else {
         if(re[1].equals("/echo/")) {
           String ec = re[1].replaceFirst("/echo/", "");
           sendCode(clientSocket, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent Length: "+re[1].length()+"\r\n\r\n"+ec);
@@ -30,8 +31,6 @@ public class Main {
         {
           sendCode(clientSocket, "HTTP/1.1 404 Not Found\r\n\r\n");
         }
-      } else {
-        sendCode(clientSocket, "HTTP/1.1 404 Not Found\r\n\r\n");
       }
 
       System.out.println("accepted new connection");
